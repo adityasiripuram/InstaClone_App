0cfd19ff2d96180ebd0a6b0774385a8bca90477b
adityasiripuram <adisiripuram007@gmail.com>
2025-02-26 15:15:02 +0530

    Updated

--- client/src/components/screens/Signin.jsx
+++ client/src/components/screens/Signin.jsx
@@ -11,7 +11,7 @@ const Signin = ()=>{
     const [email, setEmail] = useState("")
     const PostData = ()=>{
         if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
-            M.toast({html: "imvalid email", classes:"#c62828 red darken-3"})
+            M.toast({html: "invalid email", classes:"#c62828 red darken-3"})
             return
         }
         fetch("/signin", {
