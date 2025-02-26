c918990c92551f5dcf60cf3f08720d3ed2573308
adityasiripuram <adisiripuram007@gmail.com>
2025-02-26 14:33:18 +0530

    Updated

--- Server/index.js
+++ Server/index.js
@@ -2,9 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const { MONGOURI } = require("./db");
-require('dotenv').config();
 
-
+// Connect to MongoDB
 mongoose.connect(MONGOURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -16,14 +15,24 @@ mongoose.connection.on("error", (err) => {
   console.log("Mongo error", err);
 });
 
+// Require your models
 require("./models/user");
 require("./models/post");
 
+// Middlewares and routes
 app.use(express.json());
 app.use(require("./routes/auth"));
 app.use(require("./routes/post"));
 app.use(require("./routes/user"));
 
-app.listen(process.env.MONGOPORT, () => {
-  console.log("Server is listening on", process.env.MONGOPORT);
-});
+// If running locally, you might still want to listen on a port:
+if (process.env.NODE_ENV !== "production") {
+  app.listen(process.env.MONGOPORT, () => {
+    console.log("Server is listening on", process.env.MONGOPORT);
+  });
+}
+
+// For Vercel, wrap your app as a serverless function:
+const serverless = require("serverless-http");
+module.exports = app;
+module.exports.handler = serverless(app);
